e7b12ffba671385ca3eb0fad31d59f098e8dfe47
Jav1erRam0s <ramos.aranibar.javier@gmail.com>
2024-06-23 08:17:27 -0300

    add navbar

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ require("dotenv").config();
 
 let port = process.env.PORT;
 
-const path = require('path');
+const path = require("path");
 const express = require("express");
 
 const usuariosRouter = require("./routes/usuarios");
 const productosRouter = require("./routes/productos");
 const ordenesRouter = require("./routes/ordenes");
-
 
 const app = express();
 app.use(express.json());
@@ -19,11 +18,11 @@ app.use("/users", usuariosRouter);
 app.use("/products", productosRouter);
 app.use("/orders", ordenesRouter);
 
-// app.get("/", (req, res) => {
-//   res.send(`Hola desde el localhost:${port}`);
-// });
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/pages/products.html");
+// });
 
 app.listen(port, () => {
   console.log(`Servidor ejecutandose en el puerto ${port}`);
